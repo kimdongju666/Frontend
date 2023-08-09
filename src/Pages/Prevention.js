9029aa86ed51a4962840a9ae31c712c039af799b
98__dj <kimdongju666@naver.com>
2023-08-09 18:52:17 +0900

    ✅ pdf 랜더링, 마커 추가

--- src/Pages/Prevention.js
+++ src/Pages/Prevention.js
@@ -1,36 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 import Header from '../Components/Header';
 import Footer from '../Components/Footer';
 import axios from 'axios';
-import { useState } from 'react';
 
 function Solution() {
   const [pdfData, setPdfData] = useState(null);
 
-  const fetchPdf = async () => {
-    try {
-      const response = await axios.get('http://localhost:8080/pdf/download/accident.pdf', {
-        responseType: 'blob', // PDF 파일을 ArrayBuffer 형태로 받아옴
-      });
-      const pdfBlob = new Blob([response.data], { type: 'application/pdf' });
-      const pdfUrl = URL.createObjectURL(pdfBlob);
-      setPdfData(pdfUrl);
-    } catch (error) {
-      console.error('Error fetching PDF:', error);
-    }
-  };
+  useEffect(() => {
+    const fetchPdf = async () => {
+      try {
+        const response = await axios.get('http://localhost:8080/pdf/download/accident.pdf', {
+          responseType: 'blob',
+        });
+        const pdfBlob = new Blob([response.data], { type: 'application/pdf' });
+        const pdfUrl = URL.createObjectURL(pdfBlob);
+        setPdfData(pdfUrl);
+      } catch (error) {
+        console.error('Error fetching PDF:', error);
+      }
+    };
+    fetchPdf(); 
+  }, []);
 
   return (
     <>
-    <Header />
-    <div className="App" >
-      <div className="container">
-        <button onClick={fetchPdf}>Fetch PDF</button>
-        {pdfData && <iframe src={pdfData} title='PDF Viewer' type="application/pdf" width="100%" height="600px" />}
+      <Header />
+      <div className="App">
+        <div className="container">
+          {pdfData && <iframe src={pdfData} title='PDF Viewer' type="application/pdf" width="100%" height="1000px" />}
+        </div>
       </div>
-    </div>
-    <Footer />
+      <Footer />
     </>
-  )
+  );
 }
+
 export default Solution;
